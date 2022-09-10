c46dbaf72f3fb753b895f092c2a3d553ce158d0e
christiancanova <ccanova@live.com.ar>
2022-09-10 10:42:33 -0300

    concount2

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -6,52 +6,51 @@ import Button from 'react-bootstrap/Button';
 
 
 
-export default function ItemCount({quantityToAdd, setquantityToAdd, stock, initial}) {
-    const [counter, setCounter] = useState((initial));
-    const [counterStock, setCounterStock] = useState(stock);
-   
-    //increase counter
-    const increase = () => {
-      if(counter<counterStock)
-      { setCounter(count => count + 1)}
-      
-         
-    };
-   
-    //decrease counter
-    const decrease = () => {
-      if(counter>1)
+export default function ItemCount({OnAdd, quantityToAdd, setquantityToAdd, stock, initial }) {
+  const [counter, setCounter] = useState((initial));
+  const [counterStock, setCounterStock] = useState(stock);
+
+  //increase counter
+  const increase = () => {
+    if (counter < counterStock) { setCounter(count => count + 1) }
+
+
+  };
+
+  //decrease counter
+  const decrease = () => {
+    if (counter > 1)
       setCounter(count => count - 1);
-    };
+  };
 
-    
-   
-    const OnAdd = () => {
-      
-      if(counterStock>0 && counterStock>=counter) 
-      {
+
+
+  const calculoCantidad = () => {
+
+    if (counterStock > 0 && counterStock >= counter) {
       setCounterStock(count => count - (counter));
-      
-      setquantityToAdd(quantityToAdd+counter)
-        
-      };
-      
-      
+      setquantityToAdd(counter)
+      OnAdd(counter)
+
+
     };
 
-       
-    return (
-            <div>
-                <div><p>Stock Disponible: {counterStock} </p></div>
-                <div><Button variant="secondary" onClick={decrease}>-</Button>
-                <span className="counter__output">{counter}</span>
-                <Button variant="secondary" onClick={increase}>+</Button></div>
-                <div><p></p></div>
-                <div><Button variant="btn btn-outline-success" onClick={OnAdd}>Agregar al Carrito</Button></div>
-            </div>
-          );
+
+  };
+
+
+  return (
+    <div>
+      <div><p>Stock Disponible: {counterStock} </p></div>
+      <div><Button variant="secondary" onClick={decrease}>-</Button>
+        <span className="counter__output">{counter}</span>
+        <Button variant="secondary" onClick={increase}>+</Button></div>
+      <div><p></p></div>
+      <div><Button variant="btn btn-outline-success" onClick={calculoCantidad}>Agregar al Carrito</Button></div>
+    </div>
+  );
 }
-        
-       
+
+
 
 
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,35 +2,59 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Card from 'react-bootstrap/Card';
 import ItemCount from '../ItemCount/ItemCount';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
+import GrowExample from '../spinner/spinner';
 
 
 
 function ItemDetail({ item }) {
 
-const [quantityToAdd, setquantityToAdd] = useState(0);   
-    
-        
+  const [quantityToAdd, setquantityToAdd] = useState(0);
+  const [terminarCompra, setTerminarCompra] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    return (
-        <Card border="secondary" id="card2" style={{ width: '45rem', }}>
-          <Card.Img id="card-img" variant="top" src={item.thumbnail} />
-          <Card.Body>
-            <Card.Title>{item.title}</Card.Title>
-            <Card.Text>
-            Precio: ${item.price}
-            </Card.Text>
-            <ItemCount quantityToAdd={quantityToAdd} setquantityToAdd={setquantityToAdd} stock={10} initial={1}/>
-            <Card.Text>Tienes {quantityToAdd} productos en el carrito</Card.Text>
-            <div><Button as={Link} to={`/cart`} variant="btn btn-danger" >Terminar Compra</Button></div>
-          </Card.Body>
-        </Card>
-        
-        
-        
-      );
+  const OnAdd = (qty) => {
+    setTerminarCompra(true);
+    console.log(qty);
+  };
+
+  useEffect(() => {
+  const promiseObject = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  });
+  promiseObject.then(values => {
+    setLoading(false);
+  });
+
+}, )
+
+
+  return (
+
+
+    <Card border="secondary" id="card2" style={{ width: '45rem', }}>
+      <Card.Img id="card-img" variant="top" src={item.thumbnail} />
+      <Card.Body>
+        <Card.Title>{item.title}</Card.Title>
+        <Card.Text>
+          Precio: ${item.price}
+        </Card.Text>
+
+        {terminarCompra ?
+          (loading ? <div className="spinner">  <GrowExample /> </div> : (<div><Card.Text>Agregaste {quantityToAdd} productos al carrito</Card.Text> <Button as={Link} to={`/cart`} variant="btn btn-danger" >Terminar Compra</Button></div>)) :
+          (<ItemCount OnAdd={OnAdd} quantityToAdd={quantityToAdd} setquantityToAdd={setquantityToAdd} stock={10} initial={1} />)
+        }
+
+      </Card.Body>
+    </Card>
+
+
+
+  );
 }
 
 
